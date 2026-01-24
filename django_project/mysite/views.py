1fcc46ded0a0eba9a4928f727fdae1ba8379883d
Tyler Pearson <tyler.a.pear@gmail.com>
2026-01-23 23:59:25 -0800

    adds login/signup functionality

--- django_project/mysite/views.py
+++ django_project/mysite/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate
+from django.urls import reverse
 
 from xmas_lists.models import User
 
 def index(request):
-    user = User.objects.get(pk=1)
-    context = {"user": user}
-    return render(request, "mysite/index.html", context)
+    
+    return render(request, "mysite/login.html", {})
+
+def login_user(request):
+    
+    username, password = request.POST["username"], request.POST["password"]
+    user = authenticate(username=username, password=password)
+    
+    if user is not None:
+        return render(request, "mysite/index.html", {"user": user})
+    else:
+        return render(
+            request,
+            "mysite/login.html",
+            {
+                "message": "Username or password is incorrect",
+            },
+        )
+        
+def signup(request):
+    
+    username, password = request.POST["username"], request.POST["password"]
+    user = User.objects.create_user(username, "test@example.com", password)
+    
+    return HttpResponseRedirect(reverse("index"))
