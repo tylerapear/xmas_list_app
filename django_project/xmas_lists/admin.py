d25ec7a728d76e26502fbef41a5467f7dec99511
Tyler Pearson <tyler.a.pear@gmail.com>
2026-01-23 21:40:36 -0800

    removes manually created user model in ffavor of django's auth user model

--- django_project/xmas_lists/admin.py
+++ django_project/xmas_lists/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 
 from .models import User, List, ListItem
 
-admin.site.register(User)
 admin.site.register(List)
 admin.site.register(ListItem)
--- django_project/xmas_lists/models.py
+++ django_project/xmas_lists/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-class User(models.Model):
-    fullname = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.fullname
+from django.contrib.auth.models import User
     
 class List(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
