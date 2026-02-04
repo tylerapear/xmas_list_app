1269f7ce3d0cec093c88b35fc18178dc9f45d5ca
Tyler Pearson <tyler.a.pear@gmail.com>
2026-02-04 10:42:48 -0800

    uses django widget tweaks to style login page

--- django_project/mysite/settings.py
+++ django_project/mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'mysite',
     'xmas_lists',
     'guardian',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
